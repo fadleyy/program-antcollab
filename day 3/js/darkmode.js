958abd4d91e8608cc1cb8513af19b0a29b77df1d
fadleyy <user.fadli3001@gmail.com>
2023-10-02 22:16:15 +0700

    mengulang day 3

--- day 3/js/darkmode.js
+++
@@ -1,6 +0,0 @@
-const checkbox = document.querySelector('#toggle');
-const html = document.querySelector('html');
-
-checkbox.addEventListener('click', () => {
-    checkbox.checked ? html.classList.add('dark') : html.classList.remove('dark')
-})
